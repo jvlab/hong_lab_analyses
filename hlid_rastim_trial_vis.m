fa0ab886549bc932ed84a3c548040e1f5ff33977
Jonathan D Victor <jdvicto@med.cornell.edu>
2024-12-26 13:18:28 -0500

    Add files via upload
    consensus plotting of residuals seems working

--- hlid_rastim_trial_vis.m
+++ hlid_rastim_trial_vis.m
@@ -17,7 +17,8 @@
 % psg_plotcoords used for plots to enable lower-level control
 % 
 %  See also:  HLID_SETUP, HLID_LOCALOPTS, HLID_RASTIM2COORDS_DEMO,
-%  PSG_PLOTCOORDS, HLID_RASTIM_TRIAL_VIS_PLOT, HLID_RASTIM_TRIAL_VIS_LEGEND.
+%  PSG_PLOTCOORDS, HLID_RASTIM_TRIAL_VIS_PLOT, HLID_RASTIM_TRIAL_VIS_LEGEND,
+%  PROCRUSTES_CONSENSUS, PROCRUSTES_COMPAT.
 %
 hlid_setup;  %invoke hlid_localopts; set up opts_read and opts_plot
 %
@@ -30,8 +31,15 @@ nsts=length(st_labels);
 if ~exist('plot_select_list')
     plot_select_list=[1 1 1;1 1 2;2 1 1;1 2 1;2 2 1]; %combinations of sub, preproc, and st choices to plot
 end
+if ~exist('coord_lists') %combinations of dimensions to show
+    coord_lists{1}=[1 2 3];
+    coord_lists{2}=[[1 2 3];[1 2 4];[1 3 4];[2 3 4]];
+    coord_lists{3}=[2 3 4];
+end
 %
 if ~exist('opts_plot') opts_plot=struct; end
+if ~exist('opts_plot_consensus') opts_plot_consensus=opts_plot; end
+if ~exist('opts_pcon') opts_pcon=struct; end %consensus options
 %
 if ~exist('nrepts') nrepts=3; end %number of repeats
 %
@@ -62,11 +70,6 @@ if ~exist('colors') %colors for each trial
     colors={'r','m','c'};
 end
 %
-if ~exist('coord_lists') %combinations of dimensions to show
-    coord_lists{1}=[1 2 3];
-    coord_lists{2}=[[1 2 3];[1 2 4];[1 3 4];[2 3 4]];
-    coord_lists{3}=[2 3 4];
-end
 dmax=-inf;
 for icl=1:length(coord_lists)
     dmax=max(dmax,max(coord_lists{icl}(:)));
@@ -83,7 +86,7 @@ for ifile=1:nfiles
         switch sub_labels{isub}
             case ''
             case ' (mean sub)'
-            rs_xm=mean(rs,1,'omitnan'); %global mean-
+            rs_xm=mean(rs,1,'omitnan'); %global mean
             rs=rs-repmat(rs_xm,[nstims 1]);
             rt=rt-repmat(reshape(rs_xm,[1 1 nrois_avail(ifile)]),[nstims nrepts 1]);
         end
@@ -176,6 +179,81 @@ for ifile=1:nfiles
         end %ipreproc
     end %isub
 end %each file
+%
+%now tran sform into consensus across files, working with each set of coordinates
+%
+for isub=1:nsubs
+    for ipreproc=1:npreprocs
+        for ist=1:nsts
+            opts_pcon_use=opts_pcon;
+            opts_pcon_use=filldefault(opts_pcon_use,'allow_offset',1);
+            opts_pcon_use.allow_reflection=1;
+            switch preproc_labels{ipreproc}
+                case 'raw'
+                    opts_pcon_use=filldefault(opts_pcon_use,'allow_scale',1); %typically allow scaling
+                case 'normalized'
+                    opts_pcon_use.allow_scale=0; %but not if distances are already normalized to 1
+            end
+            tstring_pcon=sprintf('offset %1.0f refl %1.0f scale %1.0f',...
+                opts_pcon_use.allow_offset,opts_pcon_use.allow_reflection,opts_pcon_use.allow_scale);
+            tstring_cons=sprintf('consensus (%s): %s to %s (%2.0f files), %s %s, pca on %s',tstring_pcon,dsids{1},dsids{nfiles},nfiles,...
+                sub_labels{isub},preproc_labels{ipreproc},st_labels{ist});
+            plot_select=[isub ipreproc ist];
+            disp(tstring_cons);
+            %find consensus for each number of dimensions
+            z=zeros(nstims,dmax,nfiles); %collate the stimulus space from each file
+            for ifile=1:nfiles
+                z(:,:,ifile)=coords_stim{ifile,isub,ipreproc,ist};
+            end
+            dmax_use=size(z,2);
+            consensus=cell(1,dmax_use);
+            ts=cell(1,dmax_use); %a separate consensus for each max dimension
+            for idim=1:dmax_use
+                [consensus{idim},znew,ts{idim},details,opts_pcon_used]=procrustes_consensus(z(:,1:idim,:),opts_pcon_use);
+                %summary borrowed from psg_align_stats_demo
+                disp(sprintf(' creating Procrustes consensus for dim %2.0f, iterations: %4.0f, final total rms dev per coordinate: %8.5f',...
+                    idim,length(details.rms_change),sqrt(sum(details.rms_dev(:,end).^2))));               
+            end %idim
+            if_plot=any(all(repmat(plot_select,[size(plot_select_list,1),1])==plot_select_list,2));
+            if if_plot
+                nplots=0;
+                for icl=1:length(coord_lists)
+                    coord_list=coord_lists{icl};
+                    dplot=max(coord_list(:));
+                    if dplot<=dmax_use & size(coord_list,1)==1
+                        dimlist=coord_list(1,:);
+                        nplots=nplots+1;
+                        cons_stim=consensus{dplot}; %response consensus
+                        %compute deviation of single-trial response from its mean, after transformatoin to consensus space
+                        devs_xform_off=nan(nstims,dplot,nrepts,nfiles);
+                        for ifile=1:nfiles
+                            xform=procrustes_compat(ts{dplot}{ifile});
+                            c_stim=coords_stim{ifile,isub,ipreproc,ist}(:,1:dplot);
+                            c_stim_xform=psg_geomodels_apply('procrustes',c_stim,xform); %stimulus-mean  response in consensus space
+                            for irept=1:nrepts
+                                c_trial=coords_trial{ifile,isub,ipreproc,ist}(:,1:dplot,irept);
+                                c_trial_xform=psg_geomodels_apply('procrustes',c_trial,xform); %trial response in consensus space
+                                devs_xform_off(:,:,irept,ifile)=c_trial_xform-c_stim_xform+cons_stim; %difference, but plotted from consensus response
+                            end %irept
+                        end %ifile
+                        devs_xform_off=reshape(devs_xform_off,[nstims,dplot,nrepts*nfiles]);
+                        figure;
+                        set(gcf,'Position',[100 100 1200 800]);
+                        set(gcf,'NumberTitle','off');
+                        set(gcf,'Name',tstring_cons);
+                        [opts_plot_consensus_used,opts_plot_consensus_trial_used]=...
+                            hlid_rastim_trial_vis_plot(cons_stim,devs_xform_off,dimlist,repmat(colors,1,nfiles),...
+                            stimulus_names_display,opts_plot_consensus);
+                        axes('Position',[0.01,0.02,0.01,0.01]); %for text
+                        text(0,0,tstring_cons,'Interpreter','none');
+                        axis off;
+                   end
+                end
+            end %if_plot
+        end %ist
+    end %ipreproc
+end %isub
+
 %
 %save results
 %
