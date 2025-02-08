e6a73cbfdd16a91be1d73005caba7625ad24a2b1
Jonathan D Victor <jdvicto@med.cornell.edu>
2025-02-07 21:53:06 -0500

    Add files via upload
    also now plots line graphs of fraction correct

--- hlid_rastim_trial_decode_confmtx.m
+++ hlid_rastim_trial_decode_confmtx.m
@@ -23,12 +23,14 @@ ncols=max(results.ndecs,3);
 for isub=1:results.nsubs
     for ipreproc=1:results.npreprocs
         for isubsamp=subsamp_range(1):subsamp_range(2)
+            fcs=zeros(results.dmax,results.ndecs);
+            %plot heatmaps
             figure;
-            set(gcf,'Position',[100 100 1400 800]);
-            for irow=1:results.dmax-results.dmin+1
-                id=irow-1+results.dmin;
-                for icol=1:results.ndecs
-                    idec=icol;
+            set(gcf,'Position',[100 50 1400 950]);
+            for id=results.dmin:results.dmax
+                irow=id-results.dmin+1;
+                for idec=1:results.ndecs
+                    icol=idec;
                     subplot(nrows,ncols,icol+(irow-1)*ncols);
                     % confusion_matrices=zeros(nstims,nstims,ndecs,dmax,nsubs,npreprocs,nsubsamps_use); % d1: actual stim, d2: decoded stim, d3: decision rule, d4: dmax, d5: sub mean d6: normalize, d7: subsample set
                     if isubsamp>0
@@ -36,7 +38,7 @@ for isub=1:results.nsubs
                     else
                         confmtx=sum(results.confusion_matrices(:,:,icol,id,isub,ipreproc,:),7);
                     end
-                    fc=sum(diag(confmtx))/sum(confmtx(:));
+                    fcs(id,idec)=sum(diag(confmtx))/sum(confmtx(:));
                     imagesc(confmtx);
                     set(gca,'XTick',[1:results.nstims]);
                     set(gca,'XTickLabel',results.stimulus_names_display);
@@ -44,7 +46,7 @@ for isub=1:results.nsubs
                     set(gca,'YTickLabel',results.stimulus_names_display);
                     axis square;
                     colormap hot;
-                    title(sprintf('dim%1.0f [%s] fc %5.3f',id,results.dec_labels{idec},fc),'Interpreter','none');
+                    title(sprintf('dim%1.0f [%s] fc %5.3f',id,results.dec_labels{idec},fcs(id,idec)),'Interpreter','none');
                 end
             end
             %
@@ -63,6 +65,36 @@ for isub=1:results.nsubs
             axes('Position',[0.01,0.01,0.01,0.01]); %for text
             text(0,0,results.xv_label,'Interpreter','none');
             axis off;
+            %
+            %plot fraction correct as line graphs
+            %
+            figure;
+            set(gcf,'Position',[100 100 1200 800]);
+            %
+            plot([1:results.dmax],fcs,'LineWidth',2);
+            xlabel('dimension');
+            set(gca,'XTick',[1:results.dmax]);
+            set(gca,'XTickLabel',[1:results.dmax]);
+            set(gca,'YLim',[0 max(fcs(:))]);
+            ylabel('fraction correct');
+            legend(results.dec_labels,'Location','best');
+            %
+            axes('Position',[0.01,0.05,0.01,0.01]); %for text
+            if if_eachsubsamp
+                text(0,0,sprintf('subsamp %2.0f of %2.0f',isubsamp,nsubsamps_use),'Interpreter','none');
+            else
+                text(0,0,sprintf('all subsamps of %2.0f',nsubsamps_use),'Interpreter','none');
+            end
+            axis off;
+            %
+            axes('Position',[0.01,0.03,0.01,0.01]); %for text
+            text(0,0,sprintf('%s %s all folds, %s to %s',results.sub_labels{isub},results.preproc_labels{ipreproc},results.dsids{1},results.dsids{end}),'Interpreter','none');
+            axis off;
+            %
+            axes('Position',[0.01,0.01,0.01,0.01]); %for text
+            text(0,0,results.xv_label,'Interpreter','none');
+            axis off;
+
         end %isubsamp
     end %ipreproc
 end %isub
