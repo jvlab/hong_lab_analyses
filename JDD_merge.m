2073ed74774415ad0b032f028abdbceba18234a0
Jonathan Drover <jod2017@med.cornell.edu>
2025-10-31 13:04:26 -0400

    friday2

--- JDD_merge.m
+++ JDD_merge.m
@@ -9,7 +9,7 @@ opts.rep_char = '+';
 opts.trial_repeats = 3;
 opts.set_names = {'kiTC','meTC','moSK','vaTC'};
 opts.kdf = {'max_peak','mean_peak'}; % Known data fields.
-opts.suppressoutput = true;
+opts.suppressoutput = false;
 opts.interactive = false;
 % load the data into a structure
 % Set by set.
@@ -23,18 +23,65 @@ Sraw{4} = fileToRaw('../orn_terminals_Oct25/validation2');
 
 % Check stimulus consistency across files within a set,
 % and glomerus consistency across sets.
-[~,S] = checkConsist(Sraw,opts);
+[~,Sall] = checkConsist(Sraw,opts);
 
 % First, I want to check that each stimulus has a non-NaN value somewhere
 % within a set. If found, that stimulus is removed.
 % The presence of the glomeruli across sets is determined. the glomeruli
 % used appear in a minimum number of files. 
-S = lookForSetWideHoles(S,opts);
+Strimmed = lookForSetWideHoles(Sall,opts);
 
 % Fill in the remaining holes.
 % The calls the afalwt commands
-S = fillInNaNs(S,opts);
+Sfilled = fillInNaNs(Strimmed,opts);
 
+imagesc(Sfilled{1}{1}{:,:})
+set(gca,'XTick',[1:length(Sfilled{1}{1}.Properties.VariableNames)]);
+set(gca,'XTickLabel',Sfilled{1}{1}.Properties.VariableNames)
+
+
+% Generate the plots 
+%for setindx = 1:numSets
+%    numFiles = length(Sfilled{setindx});
+%    for ifig=1:3
+%        switch ifig
+%            case 1
+%                figname='all raw data';
+%                for fileindx = 1:numFiles
+%                    resps_plot(:,:,fileindx) = Sall{setindx}{fileindx}{:,:};
+                %glomeruli_plot=[1:nglomeruli];
+%            case 2
+%                figname='raw data from selected glomeruli';
+                %resps_plot=resps_gu;
+                %glomeruli_plot=glomeruli_use{iset};
+%            case 3
+%                figname='raw data from selected glomeruli with missing data filled in';
+                %resps_plot=resps_gu_filled;
+                %glomeruli_plot=glomeruli_use{iset};
+%        end
+%        figname=cat(2,sprintf('set %2.0f: ',iset),figname);
+%        figure;
+%        set(gcf,'Position',[50 100 1800 800]);
+%        set(gcf,'NumberTitle','off');
+%        set(gcf,'Name',figname);
+%        [nr,nc]=nicesubp(nfiles_use(iset));
+%        for ifile_ptr=1:nfiles_use(iset)
+%            ifile=files_use{iset}(ifile_ptr);
+%            subplot(nr,nc,ifile_ptr);
+%            imagesc(resps_plot(:,:,ifile_ptr));
+%            minmax=[min(min(resps_plot(:,:,ifile_ptr),[],'omitnan')),max(max(resps_plot(:,:,ifile_ptr),[],'omitnan'))];
+%            title_string=sprintf('set %1.0f file %2.0f: %s  [%6.3f %6.3f]',iset,ifile,strrep(filenames_short{iset}{ifile},'.mat',''),minmax);
+%            title(title_string,'Interpreter','none');
+%            set(gca,'FontSize',7);
+%            set(gca,'XTick',[1:length(glomeruli_plot)]);
+%            set(gca,'XTickLabel',glomeruli(glomeruli_plot));
+%            set(gca,'YTick',[1:nstims(iset)]);
+%            set(gca,'YTickLabel',stim_labels_set{iset});
+%        end
+%        axes('Position',[0.01,0.02,0.01,0.01]); %for text
+%        text(0,0,figname,'Interpreter','none');
+%        axis off;
+%    end %ifig
 
 
 
--- fillInNaNs.m
+++ fillInNaNs.m
@@ -3,25 +3,36 @@ function S = fillInNaNs(S,opts)
 
 % To use JVs code mostly as is, I need to make resps_gur using S.
 numSets = length(S);
-resps_raw = cell(numSets,1);
+resps_gur = cell(numSets,1);
+resps_tofill = cell(numSets,1);
 
 for setindx = 1:numSets
     numFiles = length(S{setindx});
     [numStim,numGlom] = size(S{setindx}{1});
-    resps_raw{setindx} = zeros(numStim,numGlom,numFiles);
-    for fileindx = 1:numSets
-        resps_raw{setindx}(:,:,fileindx)=S{setindx}{fileindx}{:,:};
+    resps_gur{setindx} = zeros(numStim*numGlom,numFiles);
+    for fileindx = 1:numFiles
+        resps_gur{setindx}(:,fileindx)=reshape(S{setindx}{fileindx}{:,:},[numStim*numGlom,1]);
+    end
+    
+    resps_tofill{setindx} = isnan(resps_gur{setindx});
+    
+    afalwt_fit=afalwt(resps_gur{setindx},1-resps_tofill{setindx},opts);
+    
+    resps_gur_fitted=(afalwt_fit.x_true*afalwt_fit.b_norm+repmat(afalwt_fit.a,size(resps_gur{setindx},1),1)); %interpolated data
+    resps_gur_filled=resps_gur{setindx};
+    resps_gur_filled(resps_tofill{setindx})=resps_gur_fitted(resps_tofill{setindx});
+    resps_gu_filled=reshape(resps_gur_filled,[numStim numGlom numFiles]);
+    
+    for fileindx = 1:numFiles
+        S{setindx}{fileindx}{:,:} = resps_gu_filled(:,:,fileindx);
+    end
+    
+    if(~opts.suppressoutput)
+        fprintf('%4.0f NaN values filled in. \n',sum(resps_tofill{setindx}(:)));
     end
 end
 
 
-
-%[afalwt_fit,afalwt_b_change,afalwt_optsused]=afalwt(resps_gur,1-resps_tofill,afalwt_opts);
-%resps_gur_fitted=(afalwt_fit.x_true*afalwt_fit.b_norm+repmat(afalwt_fit.a,size(resps_gur,1),1)); %interpolated data
-%resps_gur_filled=resps_gur;
-%resps_gur_filled(resps_tofill)=resps_gur_fitted(resps_tofill);
-%resps_gu_filled=reshape(resps_gur_filled,[nstims(iset) nglomeruli_use(iset) nfiles_use(iset)]);
-
-
 end
+
 
