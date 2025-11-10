82f30f83d76f2073d00b692adda153f96d73f586
Jonathan D Victor <jdvicto@med.cornell.edu>
2025-11-10 05:21:41 +0500

    Add files via upload
    progress on  subsampling

--- hlid_rastim_mds_coords_subsamp.m
+++ hlid_rastim_mds_coords_subsamp.m
@@ -1,8 +1,11 @@
 %hlid_rastim_mds_coords_subsamp: read calcium imaging data from Hong Lab, 
 % examine statistics of cross-prep consistency between several transformations of the z-scores
-% by doing subsamples of each set of preps
-%knit_stats
-% This runs on the quantity r saved by hlid_rastim_mds_coords_demo
+% by doing subsamples of each set of preps and extracting the variance fields from knit_stats
+% (but no shuffles are done for the subsamples)
+% 
+% This runs on the quantity r saved by hlid_rastim_mds_coords_demo,
+% and creates r_subsamp
+%
 %
 %  See also:  HLID_LOCALOPTS, HLID_RASTIM_COORDS_SUBSAMP_DEMO,
 % HLID_RASTIM_MDS_COORDS_SUMM,HLID_RASTIM_MDS_COORDS_SUMM.
@@ -10,6 +13,7 @@
 hlid_opts=hlid_localopts; %set up read_opts and plot_opts 
 %
 nfiles=length(r.filenames_short);
+nstims=r.nstims;
 nmeths=length(r.meths);
 if ~exist('subsamp_sizes') subsamp_sizes=[2 3 4 6 8]; end
 if ~exist('exhaust_max') exhaust_max=500; end
@@ -55,13 +59,6 @@ for isize=1:nsizes
     disp(sprintf('for subsample size %2.0f, %5.0f subsamples prepared',subsamp_size,size(subsamp_lists{isize},1)));
 end
 %
-r.nfiles=nfiles;
-r.nmeths=nmeths;
-r.subsamp_sizes=subsamp_sizes;
-r.nsizes=nsizes;
-r.exhaust_max=exhaust_max;
-r.dim_max_in=dim_max_in;
-r.subsamp_lists=subsamp_lists;
 %
 %align and knit with rs package
 %
@@ -75,8 +72,21 @@ aux.opts_knit.if_stats=1; %need statistics of variance
 aux.opts_knit.nshuffs=0;
 aux.opts_knit.if_plot=0; %no plotting
 aux.opts_knit.dim_max_in=dim_max_in;
+aux_subsamp=aux;
+aux_subsamp.opts_check.if_warn=0; %no need to check the subsamples
 %
 if_submean=1;
+%
+r_subsamp.nfiles=nfiles;
+r_subsamp.nstims=nstims;
+r_subsamp.nmeths=nmeths;
+r_subsamp.subsamp_sizes=subsamp_sizes;
+r_subsamp.nsizes=nsizes;
+r_subsamp.exhaust_max=exhaust_max;
+r_subsamp.dim_max_in=dim_max_in;
+r_subsamp.subsamp_lists=subsamp_lists;
+r_subsamp.sv=cell(nmeths,1+if_submean,nsizes);
+r_subsamp.sv_dims={'d1: methods, d2: submean, d3: size of subsample'};
 for isize=1:nsizes
     subsamp_size=subsamp_sizes(isize);
     nsubsamps=size(subsamp_lists{isize},1);
@@ -84,41 +94,35 @@ for isize=1:nsizes
     if nsubsamps>0
         for imeth=1:nmeths
             for submean=0:if_submean
+                sv=struct;
+                sv.subsamp_rmsdev_setwise=zeros(dim_max_in,subsamp_size,nsubsamps);
+                sv.subsamp_rmsdev_stmwise=zeros(dim_max_in,nstims,nsubsamps);
+                sv.subsamp_rmsdev_overall=zeros(dim_max_in,1,nsubsamps);
+                sv.subsamp_rmsavail_setwise=zeros(dim_max_in,subsamp_size,nsubsamps);
+                sv.subsamp_rmsavail_stmwise=zeros(dim_max_in,nstims,nsubsamps);
+                sv.subsamp_rmsavail_overall=zeros(dim_max_in,1,nsubsamps);
+                sv_fields=fieldnames(sv);
                 meth_text=cat(2,r.meths{imeth}.name_short,sprintf(' submean=%1.0f',submean));
                 disp(sprintf('processing %s',meth_text));
+                data_in=r.data{imeth,1+submean};
+                %look at all of the data, to check consistency
+                [data_align,aux_align]=rs_align_coordsets(data_in,aux);
+                [data_knit,aux_knit]=rs_knit_coordsets(data_align,aux);
                 for isubsamp=1:nsubsamps
+                    select=subsamp_lists{isize}(isubsamp,:);
+                    data_in_sub.ds=data_in.ds(select);
+                    data_in_sub.sas=data_in.sas(select);
+                    data_in_sub.sets=data_in.sets(select);
+                    [data_align_sub,aux_align_sub]=rs_align_coordsets(data_in_sub,aux_subsamp);
+                    [data_knit_sub,aux_knit_sub]=rs_knit_coordsets(data_align_sub,aux_subsamp);
+                    %
+                    for isv=1:length(sv_fields)
+                        sv_field=sv_fields{isv};
+                        sv.(sv_field)(:,:,isubsamp)=aux_knit_sub.knit_stats.(strrep(sv_field,'subsamp_',''));
+                    end
                 end
+                r_subsamp.sv{imeth,1+submean,isize}=sv;
             end %submean
         end %imeth
     end %nsubsamps>0
 end %isize
-
-
-%
-% for imeth=1:nmeths
-%     for submean=0:if_submean
-%         meth_text=cat(2,meths{imeth}.name_short,sprintf(' submean=%1.0f',submean))
-%         disp(' ');
-%         disp(sprintf('processing %s',meth_text));
-%         disp(' ');
-%         data_in=r.data{imeth,1+submean};
-%         [data_align,aux_align]=rs_align_coordsets(data_in,aux);
-%         [data_knit,aux_knit]=rs_knit_coordsets(data_align,aux);
-%         if (if_write)
-%             data_knit.sets{1}.pipeline.embedding_method=setfield(meths{imeth},'if_submean',submean);
-%             rs_write_coorddata(filenames_out{imeth,1+submean},data_knit);
-%         end
-%         r.data_knit{imeth,1+submean}=data_knit;
-%         r.knit_stats{imeth,1+submean}=aux_knit.knit_stats;
-%         %do stats if nshuffs>0
-%         if (nshuffs>0)
-%             knit_stats{imeth,1+submean}=aux_knit.knit_stats;
-%             knit_stats_setup=aux_knit.knit_stats_setup;
-%             knit_stats_setup.dataset_labels=strrep(filenames_short,'.mat','');
-%             knit_stats_setup.stimulus_labels=stim_labels;
-%             psg_knit_stats_plot(knit_stats{imeth,1+submean},knit_stats_setup);
-%             axes('Position',[0.5,0.07,0.01,0.01]); %for text
-%             text(0,0,meth_text,'Interpreter','none','FontSize',8);
-%             axis off;
-%             set(gcf,'Name',sprintf('consensus stats: %s',meth_text));
-%         end
